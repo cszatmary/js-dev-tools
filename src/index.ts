9fd4fc3f82299de101c18d46f8a4f938a7836bc6
Christopher Szatmary <cs@christopherszatmary.com>
2019-06-26 22:39:07 -0400

    Switch to tsc for compiling, ditch babel

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ var semver = require('semver');
 
 var currentNodeVersion = process.versions.node;
 
-if (semver.lt(currentNodeVersion, '6.0.0')) {
+if (semver.lt(currentNodeVersion, '8.0.0')) {
   console.log(
     chalk.red(
       `You are running Node ${currentNodeVersion}.\n` +
-      'config-gen requires Node 6 or higher.\n' +
+      'config-gen requires Node 8 or higher.\n' +
       'Please update your version of Node.',
     ),
   );
